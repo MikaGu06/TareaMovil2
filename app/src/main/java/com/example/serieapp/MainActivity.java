cb787def8ea5017dff94af7b3c7c94c9464cdbac
MikaGu06 <mikaelagutierrezurquiza@gmail.com>
2026-04-28 22:05:46 -0400

    feat: implementación de lógica matemática en Java

--- app/src/main/java/com/example/serieapp/MainActivity.java
+++ app/src/main/java/com/example/serieapp/MainActivity.java
@@ -1,24 +1,69 @@
 package com.example.serieapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etN;
+    Button btnGenerar;
+    TextView tvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etN = findViewById(R.id.etN);
+        btnGenerar = findViewById(R.id.btnGenerar);
+        tvResultado = findViewById(R.id.tvResultado);
+
+        btnGenerar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String valor = etN.getText().toString();
+
+                if (valor.isEmpty()) {
+                    etN.setError("Ingrese un valor");
+                    return;
+                }
+
+                int n;
+                try {
+                    n = Integer.parseInt(valor);
+                } catch (Exception e) {
+                    etN.setError("Solo números");
+                    return;
+                }
+
+                if (n <= 0) {
+                    etN.setError("n debe ser mayor a 0");
+                    return;
+                }
+
+                String resultado = "";
+                double suma = 0;
+
+                for (int i = 1; i <= n; i++) {
+
+                    double termino = Math.pow(-1, i + 1) * (i * i - 0.5 * (i - 1));
+
+                    resultado += termino;
+
+                    if (i < n) {
+                        resultado += ", ";
+                    }
+
+                    suma += termino;
+                }
+
+                resultado += "\nSuma: " + suma;
+
+                tvResultado.setText(resultado);
+            }
         });
     }
 }
